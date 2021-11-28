973b12eee8c85c84744fb38feb941597f0775c0b
Aarush1010 <69754168+Aarush1010@users.noreply.github.com>
2021-11-28 11:10:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,221 @@
+Num = input('Write the number:')
+
+
+def Even():
+    if Num == '1':
+        print("It's Odd")
+    elif Num == '2':
+        print("It's Even")
+    elif Num == '3':
+        print("It's Odd")
+    elif Num == '4':
+        print("It's Even")
+    elif Num == '5':
+        print("It's Odd")
+    elif Num == '6':
+        print("It's Even")
+    elif Num == '7':
+        print("It's Odd")
+    elif Num == '8':
+        print("It's Even")
+    elif Num == '9':
+        print("It's Odd")
+    elif Num == '10':
+        print("It's Even")
+    elif Num == '11':
+        print("It's Odd")
+    elif Num == '12':
+        print("It's Even")
+    elif Num == '13':
+        print("It's Odd")
+    elif Num == '14':
+        print("It's Even")
+    elif Num == '15':
+        print("It's Odd")
+    elif Num == '16':
+        print("It's Even")
+    elif Num == '17':
+        print("It's Odd")
+    elif Num == '18':
+        print("It's Even")
+    elif Num == '19':
+        print("It's Odd")
+    elif Num == '20':
+        print("It's Even")
+    elif Num == '21':
+        print("It's Odd")
+    elif Num == '22':
+        print("It's Even")
+    elif Num == '23':
+        print("It's Odd")
+    elif Num == '24':
+        print("It's Even")
+    elif Num == '25':
+        print("It's Odd")
+    elif Num == '26':
+        print("It's Even")
+    elif Num == '27':
+        print("It's Odd")
+    elif Num == '28':
+        print("It's Even")
+    elif Num == '29':
+        print("It's Odd")
+    elif Num == '30':
+        print("It's Even")
+    elif Num == '31':
+        print("It's Odd")
+    elif Num == '32':
+        print("It's Even")
+    elif Num == '33':
+        print("It's Odd")
+    elif Num == '34':
+        print("It's Even")
+    elif Num == '35':
+        print("It's Odd")
+    elif Num == '36':
+        print("It's Even")
+    elif Num == '37':
+        print("It's Odd")
+    elif Num == '38':
+        print("It's Even")
+    elif Num == '39':
+        print("It's Odd")
+    elif Num == '40':
+        print("It's Even")
+    elif Num == '41':
+        print("It's Odd")
+    elif Num == '42':
+        print("It's Even")
+    elif Num == '43':
+        print("It's Odd")
+    elif Num == '44':
+        print("It's Even")
+    elif Num == '45':
+        print("It's Odd")
+    elif Num == '46':
+        print("It's Even")
+    elif Num == '47':
+        print("It's Odd")
+    elif Num == '48':
+        print("It's Even")
+    elif Num == '49':
+        print("It's Odd")
+    elif Num == '50':
+        print("It's Even")
+    elif Num == '51':
+        print("It's Odd")
+    elif Num == '52':
+        print("It's Even")
+    elif Num == '53':
+        print("It's Odd")
+    elif Num == '54':
+        print("It's Even")
+    elif Num == '55':
+        print("It's Odd")
+    elif Num == '56':
+        print("It's Even")
+    elif Num == '57':
+        print("It's Odd")
+    elif Num == '58':
+        print("It's Even")
+    elif Num == '59':
+        print("It's Odd")
+    elif Num == '60':
+        print("It's Even")
+    elif Num == '61':
+        print("It's Odd")
+    elif Num == '62':
+        print("It's Even")
+    elif Num == '63':
+        print("It's Odd")
+    elif Num == '64':
+        print("It's Even")
+    elif Num == '65':
+        print("It's Odd")
+    elif Num == '66':
+        print("It's Even")
+    elif Num == '67':
+        print("It's Odd")
+    elif Num == '68':
+        print("It's Even")
+    elif Num == '69':
+        print("It's Odd")
+    elif Num == '70':
+        print("It's Even")
+    elif Num == '71':
+        print("It's Odd")
+    elif Num == '72':
+        print("It's Even")
+    elif Num == '73':
+        print("It's Odd")
+    elif Num == '74':
+        print("It's Even")
+    elif Num == '75':
+        print("It's Odd")
+    elif Num == '76':
+        print("It's Even")
+    elif Num == '77':
+        print("It's Odd")
+    elif Num == '78':
+        print("It's Even")
+    elif Num == '79':
+        print("It's Odd")
+    elif Num == '80':
+        print("It's Even")
+    elif Num == '81':
+        print("It's Odd")
+    elif Num == '82':
+        print("It's Even")
+    elif Num == '83':
+        print("It's Odd")
+    elif Num == '84':
+        print("It's Even")
+    elif Num == '85':
+        print("It's Odd")
+    elif Num == '86':
+        print("It's Even")
+    elif Num == '87':
+        print("It's Odd")
+    elif Num == '88':
+        print("It's Even")
+    elif Num == '89':
+        print("It's Odd")
+    elif Num == '90':
+        print("It's Even")
+    elif Num == '91':
+        print("It's Odd")
+    elif Num == '92':
+        print("It's Even")
+    elif Num == '93':
+        print("It's Odd")
+    elif Num == '94':
+        print("It's Even")
+    elif Num == '95':
+        print("It's Odd")
+    elif Num == '96':
+        print("It's Even")
+    elif Num == '97':
+        print("It's Odd")
+    elif Num == '98':
+        print("It's Even")
+    elif Num == '99':
+        print("It's Odd")
+    elif Num == '100':
+        print("It's Even")
+    elif Num == '101':
+        print("It's Odd")
+    elif Num == '102':
+        print("It's Even")
+    elif Num == '103':
+        print("It's Odd")
+    elif Num == '104':
+        print("It's Even")
+    elif Num == "105":
+        print("It's Odd")
+    elif Num == "106":
+        print("It's Even")
+    
+
+
+Even()
+
